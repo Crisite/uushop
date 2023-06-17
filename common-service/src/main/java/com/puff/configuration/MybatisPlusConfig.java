dbc58f5e7b25f2083a6a1c1453daa21e152d66da
puff <844858692@qq.com>
2023-06-17 15:49:11 +0800

    mybutispuls 分页配置

---
+++ common-service/src/main/java/com/puff/configuration/MybatisPlusConfig.java
@@ -0,0 +1,15 @@
+package com.puff.configuration;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/*
+    mybatis分页配置,没有这个配置类page.getTotal一直为0
+ */
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
