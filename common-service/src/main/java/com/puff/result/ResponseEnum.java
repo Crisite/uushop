fa29b57f61208146c2b5c9947a05dbf40f4d50cc
puff <844858692@qq.com>
2023-06-27 21:16:47 +0800

    修改响应码

--- common-service/src/main/java/com/puff/result/ResponseEnum.java
+++ common-service/src/main/java/com/puff/result/ResponseEnum.java
@@ -14,7 +14,8 @@ public enum  ResponseEnum {
     PASSWORD_ERROR(304,"密码错误"),
     TOKEN_ERROR(305,"Token失效"),
     SMS_SEND_ERROR(306,"短信发送失败"),
-    SMS_CODE_ERROR(307,"验证码错误");
+    SMS_CODE_ERROR(307,"验证码错误"),
+    ACCOUNT_ERROR(308,"帐号或密码错误");
 
     private Integer code;
     private String msg;
